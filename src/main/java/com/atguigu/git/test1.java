e476e90654357b90dec67b68021a73d7b9d3c30f
Layne <xhw@nihao.com>
2023-02-18 11:31:16 +0800

    third commit

--- src/main/java/com/atguigu/git/test1.java
+++ src/main/java/com/atguigu/git/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("git commit second");
+        System.out.println("git commit second");
     }
 }
