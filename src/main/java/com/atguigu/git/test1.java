0af45ebf68853498ec7e31024ecec09c42ca21cc
Layne <xhw@nihao.com>
2023-02-18 11:30:16 +0800

    second commit

--- src/main/java/com/atguigu/git/test1.java
+++ src/main/java/com/atguigu/git/test1.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("git commit second");
     }
 }
